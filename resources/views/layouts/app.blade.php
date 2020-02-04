c65654731ae5ea42333f601e2ecd33b26e2dca13
John Anthony Rosete <johnanthony.rosete@Personiv.com>
2020-02-04 10:33:20 +0800

    Updated Bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,9 +16,11 @@
     <script type="text/javascript" src="@yield('js')"></script>
 
     <!-- Plugins -->
-    <script type="text/javascript" src="{{ URL::asset('js/jquery-3.4.1.min.js') }}"></script>
-    <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
-    <script type="text/javascript" src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
     <div class="wrapper">
