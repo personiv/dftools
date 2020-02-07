fd08175af6efa0854106e69483f2699f936ca32e
Justin Ivan Dizon <justin.dizon@PERSONIV.COM>
2020-02-07 13:08:39 +0800

    Added cache control

---
+++ app/Http/Middleware/CacheControl.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CacheControl {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next) {
+        $response = $next($request);
+        $response->header('Cache-Control', 'private, max-age=0, no-cache, must-revalidate');
+        return $response;
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="cache-control" content="private, max-age=0, no-cache">
+    <meta http-equiv="cache-control" content="private, max-age=0, no-cache, must-revalidate">
     <meta http-equiv="pragma" content="no-cache">
     <meta http-equiv="expires" content="0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
