3172afdff8fa104dc2cbf55d28a147d7aff692bb
John Rosete <johnrosete0609@gmail.com>
2020-02-11 00:34:56 +0800

    Try

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,33 +11,52 @@
 
         <!-- Top Resource section -->
         <div class="col-md-4">
-            <div class="tb-container">
+            <div class="tb-container pb-4">
                 <div class="dboard-text  px-4 pt-4 pb-3">
                     <div class="dboard-title">Top Resource</div>
                     <div class="dboard-othtext">for the month of June</div>
                 </div>
                 <div class="dropdown-divider"></div>
                 <div class="top-resource-container">
-                    <div class="row">
+                    <div class="row remove-padding">
                         <div class="col-7">
-                            <div class="tr-section1 px-4">
-                                <i class="fa fa-user my-auto"></i>
-                                <div class="tr-name-role">
-                                    <div class="r-name">John Doe</div>
-                                    <div class="r-role">Web Designer</div>
+
+                        <!-- Name, role, scorecard title, top 1 -->
+                            <div class="tr-section-container flex-column pt-4">
+                                <div class="tr-section1 px-4 mb-4">
+                                    <i class="fa fa-user my-auto"></i>
+                                    <div class="tr-name-role">
+                                        <div class="r-name">John Doe</div>
+                                        <div class="r-role">Web Designer</div>
+                                    </div>
+                                </div>
+                                <div class="title-scorecard-container">
+                                    <div class="title-scc">
+                                        Scorecard
+                                    </div>
                                 </div>
                             </div>
+
                         </div>
-                        <div class="col-5">
-                            <div class="tr-section2 px-4">
-                                <span>1</span>
+
+                        <!-- Total score -->
+                        <div class="col-5 remove-p-left">
+                            <div class="totalScore-container flex-column p-4">
+                                <div class="total-heading">
+                                    TOTAL
+                                </div>
+                                <div class="total-score">
+                                    95%
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <!-- Progress bar, categories score, and total score -->
                 <div class="row mt-1">
-                    <div class="col-7">
-                        <div class="eachScore-container px-4 pt-0 pb-4">
+                    <div class="col">
+                        <div class="eachScore-container px-4 pt-3">
                             <!-- Productivity score -->
                             <div class="progress mt-1">
                                 <div class="progress-bar bg-success" role="progressbar" style="width: 89%" aria-valuenow="" aria-valuemin="0" aria-valuemax="100">
@@ -51,36 +70,29 @@
                             </div>
 
                             <!-- Churn score -->
-                            <span class="score-title">Churn:</span>
                             <div class="progress mt-1">
-                                <div class="progress-bar bg-warning" role="progressbar" style="width: 80%" aria-valuenow="" aria-valuemin="0" aria-valuemax="100">
-                                <span id="count-example">80%</span></div>
+                                <div class="progress-bar bg-warning" role="progressbar" style="width: 90%" aria-valuenow="" aria-valuemin="0" aria-valuemax="100">
+                                <span id="count-example">Churn: 90%</span></div>
                             </div>
 
                             <!-- Product Knowledge Test score -->
-                            <span class="score-title">Product Knowledge Test:</span>
                             <div class="progress mt-1">
                                 <div class="progress-bar bg-danger" role="progressbar" style="width: 85%" aria-valuenow="" aria-valuemin="0" aria-valuemax="100">
-                                <span id="count-example">85%</span></div>
+                                <span id="count-example">Product Knowledge Test: 85%</span></div>
                             </div>
 
                             <!-- Attendance score -->
-                            <span class="score-title">Attendance:</span>
                             <div class="progress mt-1">
                                 <div class="progress-bar bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-                                <span id="count-example">99.4%</span></div>
+                                <span id="count-example">Attendance: 100%</span></div>
                             </div>
 
-                        </div>
-                    </div>
-                    <div class="col-5">
-                        <div class="totalScore-container flex-column px-4 pb-4">
-                            <div class="total-heading">
-                                TOTAL
+                            <!-- Attendance score -->
+                            <div class="progress mt-1">
+                                <div class="progress-bar bg-danger" role="progressbar" style="width: 5%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                                <span id="count-example">Bonus: 5%</span></div>
                             </div>
-                            <div class="total-score">
-                                95%
-                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -180,14 +192,24 @@
                             <td>John Doe</td>
                             <td>Web Designer</td>
                             <td><span class="stats-for-coaching">For Coaching</span></td>
-                            <td><span id="action-btn" class="action-btn-crsession">Create Session</span></td>
+                            <td>
+                                <!-- Button trigger modal -->
+                                <a data-toggle="modal" data-target="#exampleModalCenter">
+                                    <span id="action-btn" class="action-btn-crsession">Create Session</span>
+                                </a>
+                            </td>
                         </tr>
                         <tr>
                             <td>10021002</td>
                             <td>John Doe</td>
                             <td>Web Designer</td>
                             <td><span class="stats-for-coaching">For Coaching</span></td>
-                            <td><span id="action-btn" class="action-btn-crsession">Create Session</span></td>
+                            <td>
+                                <!-- Button trigger modal -->
+                                <a data-toggle="modal" data-target="#exampleModalCenter">
+                                    <span id="action-btn" class="action-btn-crsession">Create Session</span>
+                                </a>
+                            </td>
                         </tr>
                         <tr>
                             <td>10021002</td>
