a66f07cfee2e98c888fa0fc71dedfa729680b488
John Anthony Rosete <johnanthony.rosete@Personiv.com>
2020-02-10 11:04:27 +0800

    Dashboard update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,15 +8,15 @@
     <!-- 1st row -->
     <div class="row">
 
-        <!-- Summary section -->
-        <div class="col-md-8">
-            <div class="tb-container p-4">
-                <div class="dboard-text">
+    <!-- Summary section -->
+    <div class="col-md-8">
+            <div class="tb-container">
+                <div class="dboard-text px-4 pt-4 pb-3">
                     <div class="dboard-title">Summary</div>
-                    <div class="dboard-othtext">of Lorem Ipsum</div>
+                    <div class="dboard-othtext">of <span class="stats-for-coaching">For Coaching</span>, pending and completed</div>
                 </div>
                 <div class="dropdown-divider"></div>
-                <div class="table-responsive tb-1strow-1">
+                <div class="table-responsive px-4 pt-0 pb-4">
                     <table class="table table-striped table-borderless">
                         <thead>
                         <tr>
@@ -32,50 +32,50 @@
                             <td>10021002</td>
                             <td>John Doe</td>
                             <td>Web Designer</td>
-                            <td>Not Coach</td>
-                            <td>Create Session</td>
+                            <td><span class="stats-for-coaching">For Coaching</span></td>
+                            <td><span id="action-btn" class="action-btn-crsession">Create Session</span></td>
                         </tr>
                         <tr>
                             <td>10021002</td>
                             <td>John Doe</td>
                             <td>Web Designer</td>
-                            <td>Not Coach</td>
-                            <td>Create Session</td>
+                            <td><span class="stats-for-coaching">For Coaching</span></td>
+                            <td><span id="action-btn" class="action-btn-crsession">Create Session</span></td>
                         </tr>
                         <tr>
                             <td>10021002</td>
                             <td>John Doe</td>
                             <td>Web Designer</td>
-                            <td>Not Coach</td>
-                            <td>Create Session</td>
+                            <td><span class="stats-pending">Pending</span></td>
+                            <td>N/A</td>
                         </tr>
                         <tr>
                             <td>10021002</td>
                             <td>John Doe</td>
                             <td>Web Designer</td>
-                            <td>Not Coach</td>
-                            <td>Create Session</td>
+                            <td><span class="stats-pending">Pending</span></td>
+                            <td>N/A</td>
                         </tr>
                         <tr>
                             <td>10021002</td>
                             <td>John Doe</td>
                             <td>Web Designer</td>
-                            <td>Not Coach</td>
-                            <td>Create Session</td>
+                            <td><span class="stats-completed">Completed</span></td>
+                            <td><span id="action-btn" class="action-btn-crsession">Retake Session</span></td>
                         </tr>
                         <tr>
                             <td>10021002</td>
                             <td>John Doe</td>
                             <td>Web Designer</td>
-                            <td>Not Coach</td>
-                            <td>Create Session</td>
+                            <td><span class="stats-completed">Completed</span></td>
+                            <td><span id="action-btn" class="action-btn-crsession">Retake Session</span></td>
                         </tr>
                         <tr>
                             <td>10021002</td>
                             <td>John Doe</td>
                             <td>Web Designer</td>
-                            <td>Not Coach</td>
-                            <td>Create Session</td>
+                            <td><span class="stats-completed">Completed</span></td>
+                            <td><span id="action-btn" class="action-btn-crsession">Retake Session</span></td>
                         </tr>
                         </tbody>
                     </table>
@@ -85,39 +85,40 @@
 
         <!-- Top Resource section -->
         <div class="col-md-4">
-            <div class="tb-container p-4">
-                <div class="dboard-text">
+            <div class="tb-container">
+                <div class="dboard-text  px-4 pt-4 pb-3">
                     <div class="dboard-title">Top Resource</div>
                     <div class="dboard-othtext">for the month of June</div>
                 </div>
-                <div class="top-resource-container py-4">
-                    <div><span>Name:</span>&nbsp;<span>John Doe</span></div>
-                    <div><span>Role:</span>&nbsp;<span>Web Designer</span></div>
+                <div class="dropdown-divider"></div>
+                <div class="top-resource-container px-4 py-3">
+                    <div>John Doe</div>
+                    <div>Web Designer</div>
                 </div>
                 <div class="row">
                     <div class="col-8">
-                        <div class="eachScore-container">
-                            <div class="score-title">Productivity&nbsp;<span>90%</span></div>
+                        <div class="eachScore-container px-4 pt-0 pb-4">
+                            <div class="score-title">Productivity</div>
                             <div class="progress">
                                 <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
 
-                            <div class="score-title">Quality&nbsp;<span>90%</span></div>
+                            <div class="score-title">Quality</div>
                             <div class="progress mt-1">
                                 <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                             
-                            <div class="score-title">Churn&nbsp;<span>90%</span></div>
+                            <div class="score-title">Churn</div>
                             <div class="progress mt-1">
                                 <div class="progress-bar bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
 
-                            <div class="score-title">Product Knowledge Test&nbsp;<span>90%</span></div>
+                            <div class="score-title">Product Knowledge Test</div>
                             <div class="progress mt-1">
                                 <div class="progress-bar bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
 
-                            <div class="score-title">Attendance&nbsp;<span>90%</span></div>
+                            <div class="score-title">Attendance</div>
                             <div class="progress mt-1">
                                 <div class="progress-bar bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
@@ -125,7 +126,7 @@
                         </div>
                     </div>
                     <div class="col-4">
-                        <div class="totalScore-container">
+                        <div class="totalScore-container px-4 pt-1 pb-4">
                             <div class="total-heading">
                                 TOTAL
                             </div>
@@ -145,12 +146,13 @@
 
         <!-- Exception section -->
         <div class="col-md-6">
-            <div class="tb-container p-4">
-                <div class="dboard-text">
+            <div class="tb-container">
+                <div class="dboard-text px-4 pt-4 pb-3">
                     <div class="dboard-title">Exception</div>
                     <div class="dboard-othtext">with valid reason</div>
                 </div>
-                <div class="table-responsive">
+                <div class="dropdown-divider"></div>
+                <div class="table-responsive px-4 pt-0 pb-4">
                     <table class="table table-hover table-borderless">
                         <thead>
                         <tr>
@@ -166,36 +168,36 @@
                             <td>10021002</td>
                             <td>John Doe</td>
                             <td>Web Designer</td>
-                            <td>Not Coach</td>
-                            <td>Create Session</td>
+                            <td><span class="stats-for-coaching">For Coaching</span></td>
+                            <td><span id="action-btn" class="action-btn-crsession">Create Session</span></td>
                         </tr>
                         <tr>
                             <td>10021002</td>
                             <td>John Doe</td>
                             <td>Web Designer</td>
-                            <td>Not Coach</td>
-                            <td>Create Session</td>
+                            <td><span class="stats-for-coaching">For Coaching</span></td>
+                            <td><span id="action-btn" class="action-btn-crsession">Create Session</span></td>
                         </tr>
                         <tr>
                             <td>10021002</td>
                             <td>John Doe</td>
                             <td>Web Designer</td>
-                            <td>Not Coach</td>
-                            <td>Create Session</td>
+                            <td><span class="stats-for-coaching">For Coaching</span></td>
+                            <td><span id="action-btn" class="action-btn-crsession">Create Session</span></td>
                         </tr>
                         <tr>
                             <td>10021002</td>
                             <td>John Doe</td>
                             <td>Web Designer</td>
-                            <td>Not Coach</td>
-                            <td>Create Session</td>
+                            <td><span class="stats-for-coaching">For Coaching</span></td>
+                            <td><span id="action-btn" class="action-btn-crsession">Create Session</span></td>
                         </tr>
                         <tr>
                             <td>10021002</td>
                             <td>John Doe</td>
                             <td>Web Designer</td>
-                            <td>Not Coach</td>
-                            <td>Create Session</td>
+                            <td><span class="stats-for-coaching">For Coaching</span></td>
+                            <td><span id="action-btn" class="action-btn-crsession">Create Session</span></td>
                         </tr>
                         </tbody>
                     </table>
@@ -205,12 +207,13 @@
 
         <!-- Ranking of resource section -->
         <div class="col-md-3">
-            <div class="tb-container p-4">
-                <div class="dboard-text">
+            <div class="tb-container">
+                <div class="dboard-text px-4 pt-4 pb-3">
                     <div class="dboard-title">Resource Rankking</div>
                     <div class="dboard-othtext">for the month of june</div>
                 </div>
-                <div class="table-responsive">
+                <div class="dropdown-divider"></div>
+                <div class="table-responsive px-4 pt-0 pb-4">
                     <table class="table table-borderless">
                         <thead>
                         <tr>
@@ -228,22 +231,22 @@
                         <tr>
                             <td>2</td>
                             <td>John Doe</td>
-                            <td>Logo Designer</td>
+                            <td>Custom Designer</td>
                         </tr>
                         <tr>
                             <td>3</td>
                             <td>John Doe</td>
-                            <td>Banner Designer</td>
+                            <td>Web Designer</td>
                         </tr>
                         <tr>
                             <td>4</td>
                             <td>John Doe</td>
-                            <td>Custom Designer</td>
+                            <td>Logo Designer</td>
                         </tr>
                         <tr>
                             <td>5</td>
                             <td>John Doe</td>
-                            <td>Web Designer</td>
+                            <td>Banner Designer</td>
                         </tr>
                         </tbody>
                     </table>
@@ -253,15 +256,16 @@
 
         <!-- Date plugin -->
         <div class="col-md-3">
-            <div class="tb-container p-4">
-                <div class="dboard-text">
+            <div class="tb-container">
+                <div class="dboard-text px-4 pt-4 pb-3">
                     <div class="dboard-title">Date and Time</div>
                     <div class="dboard-othtext">for the month of june</div>
                 </div>
-                
-                <div class="mt-5" id="date">Date today</div>
+                <div class="dropdown-divider"></div>
+                <div class="table-responsive px-4 pt-0 pb-4">
+                <div class="mt-3" id="date">Date today</div>
             </div>
         </div>
-
+ 
     </div>
 @endsection
