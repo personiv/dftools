cc025a86dbe627ad76a29fc1226f0537eea07294
Justin Ivan Dizon <justin.dizon@PERSONIV.COM>
2020-02-12 10:49:00 +0800

    Added json cast

--- app/Session.php
+++ app/Session.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Session extends Model
 {
     protected $primaryKey = 'session_id';
+
+    protected $casts = [
+        'session_data' => 'array'
+    ];
 }
