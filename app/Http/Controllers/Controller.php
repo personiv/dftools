17e63f353e523623d7033bd3ffbafe3f921b1b85
Justin Ivan Dizon <justin.dizon@PERSONIV.COM>
2020-02-13 10:05:00 +0800

    Moved generic helper methods to base Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,32 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
-{
+class Controller extends BaseController {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    function getFunctionName($role) {
+        switch ($role) {
+            case "DESGN": return "Web Designer";
+            case "WML": return "Web Mods Line";
+            case "VQA": return "Voice Quality Assurance";
+            case "CUSTM": return "Senior Web Designer";
+            case "PR": return "Website Proofreader";
+            default: return "N/A";
+        }
+    }
+
+    function getProficiency($date) {
+        if ($date == null) return "N/A";
+        $hireDate = date_create($date);
+        $dateNow = date_create(date("Y-m-d"));
+        $dateDifference = date_diff($hireDate, $dateNow, true);
+        $months = ((int)$dateDifference->format("%y") * 12) + (int)$dateDifference->format("%m");
+        if ($months < 3) {
+            return "Beginner (< 3 mos)";
+        } else if ($months >= 3 && $months < 6) {
+            return "Intermediate (> 3 mos)";
+        } else {
+            return "Experienced (> 6 mos)";
+        }
+    }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,32 +25,6 @@ class HomeController extends Controller {
         return array();
     }
 
-    function getProficiency($date) {
-        if ($date == null) return "N/A";
-        $hireDate = date_create($date);
-        $dateNow = date_create(date("Y-m-d"));
-        $dateDifference = date_diff($hireDate, $dateNow, true);
-        $months = ((int)$dateDifference->format("%y") * 12) + (int)$dateDifference->format("%m");
-        if ($months < 3) {
-            return "Beginner (< 3 mos)";
-        } else if ($months >= 3 && $months < 6) {
-            return "Intermediate (> 3 mos)";
-        } else {
-            return "Experienced (> 6 mos)";
-        }
-    }
-
-    function getFunctionName($role) {
-        switch ($role) {
-            case "DESGN": return "Web Designer";
-            case "WML": return "Web Mods Line";
-            case "VQA": return "Voice Quality Assurance";
-            case "CUSTM": return "Senior Web Designer";
-            case "PR": return "Website Proofreader";
-            default: return "N/A";
-        }
-    }
-
     function session(Request $r) {
         $supervisor = $r->session()->get("user");
         $agent = $r->input("session-agent");
