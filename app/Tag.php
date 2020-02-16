0edbb2e12e3ea60eb85143a6c305ab2bb632ffa5
Ivan Dizon <dizonivan11@gmail.com>
2020-02-16 12:21:13 +0800

    Added admin to EmployeeTypes

--- app/Tag.php
+++ app/Tag.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     protected $primaryKey = 'tag_id';
-    public static function EmployeeTypes() { return self::where("tag_type", "AGENT")->orWhere("tag_type", "LEADER")->get(); }
+    public static function EmployeeTypes() { return self::where("tag_type", "AGENT")->orWhere("tag_type", "LEADER")->orWhere("tag_type", "SYSTEM")->get(); }
     public static function AgentTypes() { return self::where("tag_type", "AGENT")->get(); }
     public static function LeaderTypes() { return self::where("tag_type", "LEADER")->get(); }
     public static function SessionTypes() { return self::where("tag_type", "SESSION")->get(); }
