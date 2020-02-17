4ce93f6b1ba2d00aa6a60d261ee2e12ce8a72187
Justin Ivan Dizon <justin.dizon@PERSONIV.COM>
2020-02-18 05:47:35 +0800

    Added waypoint object

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -295,6 +295,7 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="{{ URL::asset('js/jquery.waypoints.min.js') }}"></script>
     <script type="text/javascript" src="{{ URL::asset('js/progressbar.js') }}"></script>
     <!-- Global Script -->
     <script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
