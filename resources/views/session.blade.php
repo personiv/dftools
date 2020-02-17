5795a6e8a13787bb65edbc7641b95aa1bb537d00
John Anthony Rosete <johnanthony.rosete@Personiv.com>
2020-02-18 05:54:00 +0800

    Redesign session page

--- resources/views/session.blade.php
+++ resources/views/session.blade.php
@@ -17,49 +17,52 @@
 ?>
 
 @section('content')
-<div class="container mb-4">
+
+<!-- Resource's credential -->
+<div class="container-fluid mb-4">
     <div class="row">
-        <div class="col-4">
-            <div><span class="font-weight-bold">Last Name:</span> {{ $agent->FirstName() }}</div>
-            <div><span class="font-weight-bold">First Name:</span> {{ $agent->LastName() }}</div>
+        <div class="col">
+            <div><span class="font-weight-bold">Last Name:</span>&nbsp;&nbsp;{{ $agent->FirstName() }}</div>
+            <div><span class="font-weight-bold">First Name:</span>&nbsp;&nbsp;{{ $agent->LastName() }}</div>
         </div>
-        <div class="col-4">
-            <div><span class="font-weight-bold">Status:</span> {{ $agent->Status() }}</div>
-            <div><span class="font-weight-bold">Process:</span> {{ $agent->JobPosition() }}</div>
+        <div class="col">
+            <div><span class="font-weight-bold">Status:</span>&nbsp;&nbsp;{{ $agent->Status() }}</div>
+            <div><span class="font-weight-bold">Process:</span>&nbsp;&nbsp;{{ $agent->JobPosition() }}</div>
         </div>
-        <div class="col-4">
-            <div><span class="font-weight-bold">Proficiency:</span> {{ $agent->ProficiencyDetail() }}</div>
-            <div><span class="font-weight-bold">Period Covered:</span> {{ $session->DateCreated()->format('Y-m-d') }}</div>
+        <div class="col">
+            <div><span class="font-weight-bold">Proficiency:</span>&nbsp;&nbsp;{{ $agent->ProficiencyDetail() }}</div>
+            <div><span class="font-weight-bold">Period Covered:</span>&nbsp;&nbsp;{{ $session->DateCreated()->format('Y-m-d') }}</div>
         </div>
     </div>
 </div>
+
 @if (!empty($scorecard))
-<div class="container mt-4">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-12">
-            <div class="table-responsive">
+        <div class="col-lg">
+            <div class="table-responsive session-container">
                 <table id="scorecard" class="table table-bordered" style="visibility: hidden;">
                     <thead class="thead-dark">
                         <tr>
-                            <th>Classification</th>
-                            <th>Item</th>
-                            <th>Description</th>
-                            <th>Goal</th>
-                            <th>Weight</th>
-                            <th>Actual</th>
-                            <th>Overall</th>
+                            <th scope="col">Measure Classification</th>
+                            <th scope="col">Item</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Goal</th>
+                            <th scope="col">Weight</th>
+                            <th scope="col">Actual</th>
+                            <th scope="col">Overall Score</th>
                         </tr>
                     </thead>
                     <tbody>
                     @for ($i = 0; $i < count($scorecard); $i++)
                         <tr>
-                            <td class="align-middle">{{ $scorecard[$i]['score_item_class'] }}</td>
-                            <td class="align-middle">{{ $scorecard[$i]['score_item_name'] }}</td>
-                            <td style="max-width: 350px;"><pre>{{ $scorecard[$i]['score_item_desc'] }}</pre></td>
-                            <td class="align-middle">{{ $scorecard[$i]['score_item_goal'] }}</td>
-                            <td class="align-middle">{{ $scorecard[$i]['score_item_weight'] }}%</td>
-                            <td class="align-middle">{{ $scorecard[$i]['score_item_actual'] }}%</td>
-                            <td class="align-middle">{{ $scorecard[$i]['score_item_overall'] }}%</td>
+                            <td>{{ $scorecard[$i]['score_item_class'] }}</td>
+                            <td>{{ $scorecard[$i]['score_item_name'] }}</td>
+                            <td>{{ $scorecard[$i]['score_item_desc'] }}</td>
+                            <td>{{ $scorecard[$i]['score_item_goal'] }}</td>
+                            <td>{{ $scorecard[$i]['score_item_weight'] }}%</td>
+                            <td>{{ $scorecard[$i]['score_item_actual'] }}%</td>
+                            <td>{{ $scorecard[$i]['score_item_overall'] }}%</td>
                         </tr>
                     @endfor
                     </tbody>
@@ -69,28 +72,28 @@
     </div>
 </div>
 @elseif (!empty($scorecardGoal))
-<div class="container mt-4">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-12">
-            <div class="table-responsive">
+        <div class="col-lg">
+            <div class="table-responsive session-container">
                 <table id="scorecard" class="table table-bordered" style="visibility: hidden;">
                     <thead class="thead-dark">
                         <tr>
-                            <th>Classification</th>
-                            <th>Item</th>
-                            <th>Description</th>
-                            <th>Goal</th>
-                            <th>Weight</th>
+                            <th scope="col">Measure Classification</th>
+                            <th scope="col">Item</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Goal</th>
+                            <th scope="col">Weight</th>
                         </tr>
                     </thead>
                     <tbody>
                     @for ($i = 0; $i < count($scorecardGoal); $i++)
                         <tr>
-                            <td class="align-middle">{{ $scorecardGoal[$i]['score_item_class'] }}</td>
-                            <td class="align-middle">{{ $scorecardGoal[$i]['score_item_name'] }}</td>
-                            <td style="max-width: 350px;"><pre>{{ $scorecardGoal[$i]['score_item_desc'] }}</pre></td>
-                            <td class="align-middle">{{ $scorecardGoal[$i]['score_item_goal'] }}</td>
-                            <td class="align-middle">{{ $scorecardGoal[$i]['score_item_weight'] }}%</td>
+                            <td>{{ $scorecardGoal[$i]['score_item_class'] }}</td>
+                            <td>{{ $scorecardGoal[$i]['score_item_name'] }}</td>
+                            <td>{{ $scorecardGoal[$i]['score_item_desc'] }}</td>
+                            <td>{{ $scorecardGoal[$i]['score_item_goal'] }}</td>
+                            <td>{{ $scorecardGoal[$i]['score_item_weight'] }}%</td>
                         </tr>
                     @endfor
                     </tbody>
@@ -100,6 +103,94 @@
     </div>
 </div>
 @endif
+
+<!-- Target, Score Tiering -->
+<div class="container-fluid mb-4">
+    <div class="row">
+        <div class="col">
+
+        <!-- For 1st Assistant bonus score -->
+            <div class="extra-score-wrapper">
+                <div class="extra-heading">Adming Task Assignment (For 1st Assistant Only)</div>
+                <div class="table-responsive session-container">
+                    <table class="table table-bordered">
+                        <thead class="">
+                            <tr>
+                                <th scope="col">Actual</th>
+                                <th scope="col">Percentage Equivalent</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>4.01 to 5</td>
+                                <td>100%</td>
+                            </tr>
+                            <tr>
+                                <td>3.01 to 4</td>
+                                <td>80%</td>
+                            </tr>
+                            <tr>
+                                <td>2.01 to 3</td>
+                                <td>60%</td>
+                            </tr>
+                            <tr>
+                                <td>1.01 to 2</td>
+                                <td>40%</td>
+                            </tr>
+                            <tr>
+                                <td>< 1</td>
+                                <td>0%</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="extra-footer">* Agreed scoring for both OS and Designer</div>
+            </div>
+        </div>
+        <div class="col">
+
+            <!-- Churn Tiering -->
+            <div class="extra-score-wrapper">
+                <div class="extra-heading">Design Churn Tiering</div>
+                <div class="table-responsive session-container">
+                    <table class="table table-bordered">
+                        <thead class="">
+                            <tr>
+                                <th scope="col">Actual</th>
+                                <th scope="col">Percentage Equivalent</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>0%-4.99%</td>
+                                <td>100%</td>
+                            </tr>
+                            <tr>
+                                <td>5%-14.99%</td>
+                                <td>90%</td>
+                            </tr>
+                            <tr>
+                                <td>15%-24.99%</td>
+                                <td>80%</td>
+                            </tr>
+                            <tr>
+                                <td>25%-29.99%</td>
+                                <td>70%</td>
+                            </tr>
+                            <tr>
+                                <td>> = 30% </td>
+                                <td>0%</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="extra-footer">* Agreed scoring for both OS and Designer</div>
+            </div> 
+        </div>
+        <div class="col"></div>
+    </div>
+</div>
+
 @foreach ($fields as $fieldName => $fieldProperties)
 <?php
     $field_title = $fieldProperties["title"];
@@ -108,7 +199,7 @@
     $field_for = $fieldProperties["for"];
     $field_pending = $fieldProperties["pending"];
 ?>
-<div class="container mt-4">
+<div class="container-fluid mt-4">
     <div class="row">
         <div class="col-{{ $field_size }}">
             <div class="card">
@@ -125,7 +216,7 @@
     </div>
 </div>
 @endforeach
-<div class="container mt-5">
+<div class="container-fluid mt-5">
     <div class="row">
         @foreach ($signees as $employeeID => $signed)
             <?php $employee = App\Credential::where("credential_user", $employeeID)->first() ?>
