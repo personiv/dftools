403418879c0b387ecfeeb47dd1dc319638202f77
Justin Ivan Dizon <justin.dizon@PERSONIV.COM>
2020-02-19 03:02:21 +0800

    Revised score item model

--- database/migrations/2020_02_04_072846_create_score_items_table.php
+++ database/migrations/2020_02_04_072846_create_score_items_table.php
@@ -18,7 +18,7 @@ class CreateScoreItemsTable extends Migration
             $table->string('score_item_role', 5);
             $table->string('score_item_class', 64);
             $table->string('score_item_name', 64);
-            $table->string('score_item_desc');
+            $table->string('score_item_desc', 10240);
             $table->string('score_item_title', 128);
             $table->string('score_item_goal', 64);
             $table->integer('score_item_weight');
@@ -43,10 +43,10 @@ class CreateScoreItemsTable extends Migration
 
         addItem("DESGN", "Quantitative Measure (95%)", "Productivity Rate", "Productivity Score (BAU, MODs, etc)", "Productivity", "10 pts per day", 25, "W");
         addItem("DESGN", "Quantitative Measure (95%)", "Quality", "Design Quality Scores (BAU) - from PR\nDesign Quality Scores (MODs) - from PR", "Quality", "80%", 10, "Y");
-        addItem("DESGN", "Quantitative Measure (95%)", "Efficiency", "Design Churn", "Refer to Tier", "Churn", 25, "X");
-        addItem("DESGN", "Quantitative Measure (95%)", "Efficiency", "Attendance Rate (actual score - individual)", "95%", "Attendance", 15, "AB");
-        addItem("DESGN", "Quantitative Measure (95%)", "Product Knowledge", "Product Knowledge Test (actual score)", "80%", "PKT", 20, "AC");
-        addItem("DESGN", "Qualitative Measure (5%)", "Bonus", "1. Admin task assignments (1st & 2nd assistants only)\n2. Commendation from the client\n3. Issue identifier (client-approved)\n4. Issue resolver (client-approved)\n5. Innovation ideas implemented on a Personiv (approved by Paulo) and/or DexYP level (approved by client).\n6. OM Initiated (core-team approved)", "Met 1", "Bonus", 5, "AF");
+        addItem("DESGN", "Quantitative Measure (95%)", "Efficiency", "Design Churn", "Churn", "Refer to Tier", 25, "X");
+        addItem("DESGN", "Quantitative Measure (95%)", "Efficiency", "Attendance Rate (actual score - individual)", "Attendance", "95%", 15, "AB");
+        addItem("DESGN", "Quantitative Measure (95%)", "Product Knowledge", "Product Knowledge Test (actual score)", "PKT", "80%", 20, "AC");
+        addItem("DESGN", "Qualitative Measure (5%)", "Bonus", "1. Admin task assignments (1st & 2nd assistants only)\n2. Commendation from the client\n3. Issue identifier (client-approved)\n4. Issue resolver (client-approved)\n5. Innovation ideas implemented on a Personiv (approved by Paulo) and/or DexYP level (approved by client).\n6. OM Initiated (core-team approved)", "Bonus", "Met 1", 5, "AF");
     }
 
     /**
