127c3889b56437f18abcba43dff9bdac1e9dd9e1
Justin Ivan Dizon <justin.dizon@PERSONIV.COM>
2020-02-25 00:11:26 +0800

    Cleaned spaces

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,8 +25,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
     <link rel="stylesheet" href="{{ URL::asset('css/bootstrap-datepicker3.min.css') }}">
-    
-    
 </head>
 <body>
 
