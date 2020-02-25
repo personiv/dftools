0c83fa5713c1936e6300af314b86339e25248ed7
Justin Ivan Dizon <justin.dizon@PERSONIV.COM>
2020-02-25 22:35:21 +0800

    Fixed date input

--- public/js/history.js
+++ public/js/history.js
@@ -1,6 +1,8 @@
-// Datepicker for history page
-$('#history-wrapper .input-daterange').datepicker({
-  clearBtn: true,
-  autoclose: true,
-  todayHighlight: true
-});
+window.onload = function() {
+  // Datepicker for history page
+  $('#history-wrapper .input-daterange').datepicker({
+    clearBtn: true,
+    autoclose: true,
+    todayHighlight: true
+  });
+}
